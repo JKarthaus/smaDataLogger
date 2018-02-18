b12830cc09859c84e43094d204dcec202629483b
jkarthaus <joern.karthaus@gmail.com>
2018-02-18 11:14:57 +0100

    add some more Info to install Script

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+echo "Install ->                                                                       "
+echo "		      _            _____        _        _                                 "                                 
+echo "           | |          |  __ \      | |      | |                                "                               
+echo "  ___  ___ | | __ _ _ __| |  | | __ _| |_ __ _| |     ___   __ _  __ _  ___ _ __ " 
+echo " / __|/ _ \| |/ _` | '__| |  | |/ _` | __/ _` | |    / _ \ / _` |/ _` |/ _ \ '__|"
+echo " \__ \ (_) | | (_| | |  | |__| | (_| | || (_| | |___| (_) | (_| | (_| |  __/ |   "
+echo " |___/\___/|_|\__,_|_|  |_____/ \__,_|\__\__,_|______\___/ \__, |\__, |\___|_|   "
+echo "                                                            __/ | __/ |          "
+echo "                                                           |___/ |___/           "
+echo " "
+echo "Check Ip Adresse in File: Ansible/hosts "
+echo "Check remote_user setting in File Ansible/ansible.cfg"
+echo " "
+read -p "Press any key to continue... " -n1 -s
+
 ./gradlew build
 
 cd Ansible
