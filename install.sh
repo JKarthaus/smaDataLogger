8b63baaa337cfcaaf3b41f76b29e8ea7af9de66d
jkarthaus <joern.karthaus@gmail.com>
2018-02-18 12:01:44 +0100

    add more infos for simpler install process

--- install.sh
+++ install.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
+
+
 echo "Install ->                                                                       "
-echo "		      _            _____        _        _                                 "                                 
-echo "           | |          |  __ \      | |      | |                                "                               
-echo "  ___  ___ | | __ _ _ __| |  | | __ _| |_ __ _| |     ___   __ _  __ _  ___ _ __ " 
-echo " / __|/ _ \| |/ _` | '__| |  | |/ _` | __/ _` | |    / _ \ / _` |/ _` |/ _ \ '__|"
-echo " \__ \ (_) | | (_| | |  | |__| | (_| | || (_| | |___| (_) | (_| | (_| |  __/ |   "
-echo " |___/\___/|_|\__,_|_|  |_____/ \__,_|\__\__,_|______\___/ \__, |\__, |\___|_|   "
-echo "                                                            __/ | __/ |          "
-echo "                                                           |___/ |___/           "
+echo " ######   #######  ##          ###    ########  ########     ###    ########    ###    ##        #######   ######    ######   ######## ########   "                                 
+echo "##    ## ##     ## ##         ## ##   ##     ## ##     ##   ## ##      ##      ## ##   ##       ##     ## ##    ##  ##    ##  ##       ##     ##  "                               
+echo "##       ##     ## ##        ##   ##  ##     ## ##     ##  ##   ##     ##     ##   ##  ##       ##     ## ##        ##        ##       ##     ##  " 
+echo " ######  ##     ## ##       ##     ## ########  ##     ## ##     ##    ##    ##     ## ##       ##     ## ##   #### ##   #### ######   ########   "
+echo "      ## ##     ## ##       ######### ##   ##   ##     ## #########    ##    ######### ##       ##     ## ##    ##  ##    ##  ##       ##   ##    "
+echo "##    ## ##     ## ##       ##     ## ##    ##  ##     ## ##     ##    ##    ##     ## ##       ##     ## ##    ##  ##    ##  ##       ##    ##   "
+echo " ######   #######  ######## ##     ## ##     ## ########  ##     ##    ##    ##     ## ########  #######   ######    ######   ######## ##     ##  "
+echo "1. check that you habe gradle installed "
+echo "2. check Ip Adresse in File: Ansible/hosts "
+echo "3. check remote_user setting in File Ansible/ansible.cfg"
 echo " "
-echo "Check Ip Adresse in File: Ansible/hosts "
-echo "Check remote_user setting in File Ansible/ansible.cfg"
-echo " "
-read -p "Press any key to continue... " -n1 -s
+read -p "Press any key to continue... " bla
 
 ./gradlew build
 
 cd Ansible
 
-ansible-playbook --ask-sudo-pass smaDataLogger.yml
+# ansible-playbook --ask-sudo-pass smaDataLogger.yml
+ansible-playbook smaDataLogger.yml
