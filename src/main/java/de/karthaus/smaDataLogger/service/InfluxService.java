75f08f0487651ab2093f9e0083a08ef11142c5c9
jkarthaus <joern.karthaus@gmail.com>
2020-04-13 11:09:50 +0200

    Do many cleanup from old Eclipse Project Structure Add tests for Influx connection

--- src/main/java/de/karthaus/smaDataLogger/service/InfluxService.java
+++ src/main/java/de/karthaus/smaDataLogger/service/InfluxService.java
@@ -1,8 +1,15 @@
 package de.karthaus.smaDataLogger.service;
 
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 @ConditionalOnProperty(
@@ -11,15 +18,27 @@ import org.springframework.stereotype.Component;
         matchIfMissing = false)
 public class InfluxService {
 
-    @Value("${infux.db.server}")
+    private static final Logger log = LoggerFactory.getLogger(InfluxService.class);
+
+    @Value("${influx.db.server:'http://127.0.0.1:8086'}")
     private String server;
 
-    @Value("${infux.db.username}")
+    @Value("${influx.db.username:username}")
     private String username;
 
-    @Value("${infux.db.password}")
+    @Value("${influx.db.password:password}")
     private String password;
 
+    private final static String DATABASE_NAME = "smaDataLogger";
+
+    @PostConstruct
+    public void init() {
+        log.info("Try to connect to InfluxDB Server {} with userName:{}", server, username);
+        InfluxDB influxDB = InfluxDBFactory.connect(server, username, password);
+
+        influxDB.query(new Query("CREATE DATABASE " + DATABASE_NAME));
+        influxDB.setDatabase(DATABASE_NAME);
+    }
 
     public void WriteDatapoint() {
 
